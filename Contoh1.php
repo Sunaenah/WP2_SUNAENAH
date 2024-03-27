e09da6db66964d91fd769bbd66952a3d2f385a4d
sunaenah <120291587+Sunaenah@users.noreply.github.com>
2024-03-26 19:27:06 -1200

    Add files via upload

---
+++ Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Sunaenah
+        saya tinggal di Cibitung
+        olahraga yang saya sukai adalah
+        Rebahan";
+    }
+}
